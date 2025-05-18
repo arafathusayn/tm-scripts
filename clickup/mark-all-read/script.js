def85455f5a2669dcb4a98f53c219a05e117172f
Arafat Husayn <20070310+arafathusayn@users.noreply.github.com>
2025-05-18 16:24:37 +0600

    Initial commit

---
+++ clickup/mark-all-read/script.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         ClickUp - Mark All Read
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-18
+// @description  Mark all unread notifications read on ClickUp
+// @author       Arafat Husayn <hello@arafat.dev>
+// @match        https://*.clickup.com/*/inbox*
+// @run-at       context-menu
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=clickup.com
+// @grant        none
+// @copyright    2025 Arafat Husayn, GNU Affero General Public License v3.0
+// @source       https://github.com/arafathusayn/tm-scripts/blob/main/clickup/mark-all-read/script.js
+// ==/UserScript==
+
+(() => {
+  "use strict";
+
+  const READ_SELECTOR = '[aria-label="Mark as read"]';
+  const BUNDLE_SELECTOR = "cu3-notification-bundle-actions";
+
+  window.actionElements = [];
+
+  document.querySelectorAll(READ_SELECTOR).forEach((button) => {
+    const bundle = button.closest(BUNDLE_SELECTOR);
+    if (bundle) window.actionElements.push(bundle);
+    button.click();
+  });
+})();
---
+++ clickup/undo-mark-all-read/script.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         ClickUp - Undo Mark All Read
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-18
+// @description  Undo mark all read notifications on ClickUp
+// @author       Arafat Husayn <hello@arafat.dev>
+// @match        https://*.clickup.com/*/inbox*
+// @run-at       context-menu
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=clickup.com
+// @grant        none
+// @copyright    2025 Arafat Husayn, GNU Affero General Public License v3.0
+// @source       https://github.com/arafathusayn/tm-scripts/blob/main/clickup/undo-mark-all-read/script.js
+// ==/UserScript==
+
+(() => {
+  "use strict";
+
+  const UNREAD_SELECTOR = '[aria-label="Mark as unread"]';
+
+  window.actionElements.forEach((bundle) => {
+    const btn = bundle.querySelector(UNREAD_SELECTOR);
+    btn?.click();
+  });
+})();
